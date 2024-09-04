a12538c1ca4cda9709bad847a5699f5998f8dc10
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-09-04 15:56:39 +0530

    concepts

---
+++ demo.java
@@ -0,0 +1,11 @@
+public class demo {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = (float) a;
+
+        int c = 345;
+        byte d = (byte)c; 
+        System.out.println(b);
+        System.out.println(d);
+    }
+}
